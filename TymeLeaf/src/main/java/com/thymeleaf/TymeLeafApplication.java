68f944f6f2e2e6587f03e988b3c62ace9107a456
KimYJInComapny <agida0413>
2024-10-21 17:23:56 +0900

    'thymeleaf'

---
+++ TymeLeaf/src/main/java/com/thymeleaf/TymeLeafApplication.java
@@ -0,0 +1,14 @@
+package com.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymeLeafApplication.class, args);
+
+	}
+
+}
---
+++ TymeLeaf/src/test/java/com/thymeleaf/TymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
